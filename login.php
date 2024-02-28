6c4caf7c5fbc00aaef4a61ebe9b950f18fc8125c
DDStrw <satriodandung00@gmail.com>
2024-02-28 10:27:34 +0700

    Perubahan Bagian Login

--- login.php
+++ login.php
@@ -32,7 +32,7 @@
         </div>
         <div class="row">
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
           </div>
           <!-- /.col -->
         </div>
