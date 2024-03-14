1c03a5136be444a75c6cacf882131abe5f7b05d9
Aryanando Mac <aryanando232@gmail.com>
2024-03-14 13:16:52 +0700

    Fixing Data Table

--- pasienranap.php
+++ pasienranap.php
@@ -199,7 +199,7 @@ $user = $_SESSION["user_data"];
                   <td><?= $dataPasien['kdbgs'] ?></td>
                   <td><?= $dataPasien['alamat'] . ', ' . $dataPasien['kel'] . ', ' . $dataPasien['kec'] . ', ' . $dataPasien['kab'] . ', ' . $dataPasien['prov'] ?></td>
                   <td><?= $dataPasien['pj'] ?></td>
-                  <?= $user['role'] == 'gizi' ? '<td><a href="' . getKuesionerURL() . 'kuesioner/kepuasan-pasien-gizi/' . str_replace("/", "", $dataPasien['no_rawat']) . '?nama=' . $dataPasien['nama'] . '&bgsl=' . $dataPasien['bgsl'] . '" type="button" class="btn btn-primary">Review Makanan</a><td>' : ''  ?>
+                  <?= $user['role'] == 'gizi' ? '<td><a href="' . getKuesionerURL() . 'kuesioner/kepuasan-pasien-gizi/' . str_replace("/", "", $dataPasien['no_rawat']) . '?nama=' . $dataPasien['nama'] . '&bgsl=' . $dataPasien['bgsl'] . '" type="button" class="btn btn-primary">Review Makanan</a></td>' : ''  ?>
 
                 </tr>
               <?php
